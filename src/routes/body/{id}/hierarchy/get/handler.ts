547f0bf690c0da5d02e14540944f0c6aacf5e514
ipatchi <ipatchilives@gmail.com>
2024-06-18 14:10:27 +0100

    introduce the working hierarchy handler so that it returns a hierarchy, may be inneficient though.

--- src/routes/body/{id}/hierarchy/get/handler.ts
+++ src/routes/body/{id}/hierarchy/get/handler.ts
@@ -8,77 +8,44 @@ const handler = async (req: ExpressRequest, res: ExpressResponse) => {
   //Retrieve Body using ID from database
   const body = await getItem(id);
 
-  //Get all parents
   let hierarchy = { [id]: null };
 
+  //Get children (recursive) function
+  const getChildren = async (id: string) => {
+    let children = await searchItem({
+      parentBodyId: id,
+    });
+
+    let childrenObject = {};
+
+    for (let i = 0; i < children.length; i++) {
+      const child = children[i];
+      const childID = child._id.toString();
+      let childrensChildren = await getChildren(childID);
+      childrenObject[childID] = childrensChildren;
+    }
+
+    if (Object.keys(childrenObject).length > 0) {
+      return childrenObject;
+    }
+    return null;
+  };
+
+  hierarchy[id] = await getChildren(id);
+
+  //Add parents to front of hierarchy
   let hasParent = true;
   let lookupId = id;
-  let path = '';
   while (hasParent) {
     const body = await getItem(lookupId);
     const parentId = body.parentBodyId;
     if (parentId) {
       hierarchy = { [parentId]: hierarchy };
-      path = parentId + (path ? `.${path}` : '');
       lookupId = parentId;
     } else {
       hasParent = false;
     }
   }
-
-  //Output IDs
-  console.log('hierarchy: ', hierarchy);
-  console.log('path: ', path);
-
-  const getChildren = async (id: string, objectPath: string) => {
-    const splitPath = objectPath.split('.');
-    let item = hierarchy;
-    splitPath.forEach((pathItem) => {
-      console.log('item:', item);
-      console.log('path item: ', pathItem);
-      item = item[pathItem];
-    });
-    console.log('item is called:', item);
-    console.log('PID:', item);
-    let children = await searchItem({
-      parentBodyId: id,
-    });
-
-    children.forEach((child) => {
-      const childID = child._id.toString();
-      console.log(childID);
-      console.log(child.name);
-      hierarchy = { [hierarchy] : [childID] };
-      console.log(hierarchy);
-    });
-
-    //getChildren(childID, objectPath+.id)
-  };
-
-  await getChildren(id, path);
-  //Recursively search all records where parentsID = param ID
-
-  //Return all records
-
-  //Look at mongoDB indexes
-
-  //Create names from IDs (for ease of checking functionality)
-  // const hierarchyNameList = [];
-  // let index = 0;
-  // while (index < hierarchyList.length) {
-  //   const name = (await getItem(hierarchyList[index])).name;
-  //   hierarchyNameList.push(name);
-  //   index++;
-  // }
-  // console.log(hierarchyNameList);
-
-  // //Create and return the response
-  // const resp = hierarchyNameList;
-  // if (!resp) {
-  //   let message = 'Item not found, Parent: ' + hierarchyList[1];
-  //   return res.status(404).send({ message: message });
-  // }
-
   return res.status(200).send(hierarchy);
 };
 
